edfd0c915881c6a7563bc24f09674ad3300daab3
paulrwd85@gmail.com <paulrwd85@gmail.com>
2021-03-05 11:18:28 +0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using HtmlAgilityPack;
+using System.Data;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using FinParser;
+
+namespace Fin_pars
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Net.WebClient wc = new System.Net.WebClient();
+            HtmlDocument html = new HtmlDocument();
+
+
+            for (int i = 0; ;i++)
+            {
+                Console.WriteLine("SBER : " + FinParser.Parse.NowPrice("SBER.ME", ref wc, ref html) + " : " + DateTime.Now);
+                Console.WriteLine("GAZP : " + FinParser.Parse.NowPrice("GAZP.ME", ref wc, ref html) + " : " + DateTime.Now);
+                Console.WriteLine("TSLA : " + FinParser.Parse.NowPrice("TSLA", ref wc, ref html) + " : " + DateTime.Now);
+                Console.WriteLine("ALRS : " + FinParser.Parse.NowPrice("ALRS.ME", ref wc, ref html) + " : " + DateTime.Now);
+            }
+            
+            Console.ReadLine();
+
+        }
+    }
+}
